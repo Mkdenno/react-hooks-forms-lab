6b3d7a461f5ebcd3ed6d520c9adc845d623d9dac
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-07-17 20:10:16 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,15 @@ function App() {
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  function handleItemSubmit(newItem){
+    setItems([...items, newItem])
+  }
+  
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList onItemFormSubmit={handleItemSubmit} items={items}/>
     </div>
   );
 }
